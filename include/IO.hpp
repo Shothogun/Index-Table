8b75ad593b011a83f1f9c9f4251a4938bbc0a65b
GiordanoSM <33360879+GiordanoSM@users.noreply.github.com>
2018-05-18 17:05:46 -0300

    Changes 18/05 Sexta a tarde

---
+++ include/IO.hpp
@@ -0,0 +1,14 @@
+#ifndef IO_H
+#define IO_H
+
+#include <string>
+
+using namespace std;
+
+void show_primary_index (string file_name);
+
+void show_secondary_index (string file_name_keys, string file_name_labels);
+
+void show_registers (string file_name);
+
+#endif
--- include/sorting.hpp
+++ include/sorting.hpp
@@ -2,17 +2,18 @@
 #define SORTING_H
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
-void insertion_sort (vector <int> &unsorted_values);
+void insertion_sort (vector <string> &unsorted_values);
 
-void insertion_sort_inclusion (vector <int> &sorted_values);
+void insertion_sort_inclusion (vector <string> &sorted_values, int number_insertions);
 
-void heapsort (vector <int> &unsorted_values);
+void heapsort (vector <string> &unsorted_values);
 
-void max_heapfy (vector<int> &unsorted_values, int current_node, int size);
+void max_heapfy (vector<string> &unsorted_values, int current_node, int size);
 
-void create_max_heap (vector<int> &unsorted_values);
+void create_max_heap (vector<string> &unsorted_values);
 
 #endif
